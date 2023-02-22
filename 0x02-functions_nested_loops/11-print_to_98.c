97f13d41cc57dc14161e676cf72f8415e4fe8052
JaruMoropane <jaru.moropane@gmail.com>
2023-02-22 02:28:48 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+		int i, j;
 
 
 		if (n <= 98)
@@ -21,7 +21,7 @@ void print_to_98(int n)
 				else if (i == 98)
 					printf("%d\n", i);
 			}
-		}else if (n >= 98)
+		} else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
 			{
@@ -29,10 +29,6 @@ void print_to_98(int n)
 					printf("%d, ", j);
 				else if (j == 98)
 					printf("%d\n", j);
-
 			}
-
 		}
-
 }
-
