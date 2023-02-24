04157831ea08c35a1b7a2a257e66d94561c30218
JaruMoropane <jaru.moropane@gmail.com>
2023-02-24 09:10:41 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
@@ -6,22 +6,23 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	 if (n <= 0)
+	 {
+		  _putchar('\n');
+	 } else
+	 {
+		 int i, j;
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				 if (j == i)
-					 _putchar('\\');
-				 else if (j < i)
-					 _putchar(' ');
-			}
-		}
-	}
+            for (i = 0; i < n; i++)
+            {
+            for (j = 0; j < n; j++)
+            {
+            if (j == i)
+            _putchar('\\');
+            else if (j < i)
+            _putchar(' ');
+             }
+             _putchar('\n');
+	    }
+	 }
 }
